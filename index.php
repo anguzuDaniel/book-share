d7c4f5fe5f2f8a0cad38c37e8c17ca6386c96234
anguzuDaniel <102733205+anguzuDaniel@users.noreply.github.com>
2022-11-21 16:10:41 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h1>Book Share</h1>
+
+        <nav>
+            <a href="">All books</a>
+            <a href="">Education</a>
+            <a href="">Thriller</a>
+            <a href="">Adventure</a>
+        </nav>
+
+        <div class="header__cta">
+            <a href="">Login</a>
+            <a href="">Register</a>
+        </div>
+    </header>
+
+    <main>
+        <section class="hero">
+            <h2>Share your books with the world</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+            <a href="">Register</a>
+        </section>
+    </main>
+</body>
+
+</html>
